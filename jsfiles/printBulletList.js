b561ae0267ef73d77975088fd6cf596b90e3c8d0
waqargithub <waqar.mahmood.pro@gmail.com>
2018-03-31 21:15:34 -0500

    Tweaked bulleting. Fixed bug with programming list.

--- jsfiles/printBulletList.js
+++ jsfiles/printBulletList.js
@@ -17,7 +17,7 @@
 
       //If no of cols requested exceeds no. of bullets then set no of cols to no. of bullets  
 
-        if (bulletArray.length < noOfColsRequested) {
+        if (bulletArray.length < noOfCols) {
           noOfCols = bulletArray.length;
         }
 
@@ -29,7 +29,7 @@
           
        //Write html for heading, followed by unordered list column by column
         document.write("<div class='wrapper'>");  
-        document.write("<p style='margin-top: 0px; margin-bottom: 0px;'>");
+//        document.write("<p style='margin-top: 0px; margin-bottom: 0px;'>");
         
         //Heading level 0 is used when there is no heading, and no space desired between successive
         //columns of bullets
@@ -37,22 +37,16 @@
           document.write("<h"+headingLevel+">"+heading+"</h"+headingLevel+">");
         }
         for (i=0; i<noOfCols; i++) {
-/*          if (headingLevel > 0)
-            document.write("<ul style='float: left; top-margin: 0; bottom-margin: 0;'>");
-          else
-            document.write("<ul style='float: left; top-margin: 0; bottom-margin: 0;'>");*/
           document.write("<ul style='float: left; top-margin: 0; bottom-margin: 0;'>");
           for (j=0; j< noOfRows; j++) {
             //since last row may not be fully populated, do not write if reached end of bullet array
-            if ((i + noOfCols*j) < bulletArray.length)
+            if ( (i + noOfCols*j) < bulletArray.length )
               document.write("<li>"+bulletArray[i+noOfCols*j]+"</li>");
-                }
+          }
           document.write("</ul>");
         }
-        document.write("</p>");
+//        document.write("</p>");
         document.write("</div>");
-        if (headingLevel>0) {
-          document.write("<div style='clear:both; margin-top: 0px; margin-bottom: 0px; height: 0px;'>&nbsp;</div>");
-        }
+        document.write("<div style='clear:both; margin-top: 0px; margin-bottom: 0px; height: 0px;'>&nbsp;</div>");
         
       }
